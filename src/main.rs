6880b604307cb0584a77edfa1874cdaaa0c19c65
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-10-07 04:15:32 +0530

    Create ReplayInfo struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rust_strings::FileConfig;
 use std::collections::HashSet;
 use std::env;
+use std::error::Error;
 use std::ffi::CString;
 use std::path::PathBuf;
-use std::error::Error;
 
 use serde::{Deserialize, Serialize};
 
@@ -31,21 +31,19 @@ struct Replay {
 
 impl Replay {
     fn new(path: PathBuf) -> Self {
-        Self {
-            path: path,
-        }
+        Self { path }
     }
 
     unsafe fn decompress(&self, output: PathBuf) -> Result<DecompressedReplay, Box<dyn Error>> {
         let input_file = CString::new(
-            self.path.to_str().ok_or(
-                format!("CString fail: {:?}", self.path)
-            )?
+            self.path
+                .to_str()
+                .ok_or(format!("CString fail: {:?}", self.path))?,
         )?;
         let output_file = CString::new(
-            output.to_str().ok_or(
-                format!("CString fail: {:?}", output)
-            )?
+            output
+                .to_str()
+                .ok_or(format!("CString fail: {:?}", output))?,
         )?;
         unsafe {
             decompress_replay_file(input_file.as_ptr(), output_file.as_ptr());
@@ -55,19 +53,61 @@ impl Replay {
 }
 
 #[derive(Debug, Clone)]
-struct DecompressedReplay {
-    path: PathBuf,
+struct ReplayInfo {
     pub player_names: HashSet<String>,
     pub team_names: HashSet<String>,
-}    
+}
+
+impl ReplayInfo {
+    fn new(strings: Vec<(String, u64)>) -> Self {
+        let mut player_names = HashSet::new();
+        let mut team_names = HashSet::new();
+
+        for (string, _) in strings {
+            if string.starts_with("{\"v\":") {
+                Self::process_player_name(string)
+                    .map(|player_name| player_names.insert(player_name));
+            } else if string.starts_with("{\"t\":[\"") {
+                Self::process_team_name(string).map(|team_name| team_names.insert(team_name));
+            }
+        }
+
+        Self {
+            player_names,
+            team_names,
+        }
+    }
+
+    fn process_player_name(string: String) -> Option<String> {
+        let player: Result<Player, _> = serde_json::from_str(&string);
+        player
+            .ok()
+            .and_then(|player| match player.name.ends_with("...") {
+                true => None,
+                false => Some(player.name),
+            })
+    }
+
+    fn process_team_name(string: String) -> Option<String> {
+        let team: Result<Team, _> = serde_json::from_str(&string);
+        team.ok().and_then(|mut team| match team.name[0].as_str() {
+            "teamNames" => {
+                let team_name = team.name.pop().unwrap();
+                Some(team_name)
+            }
+            _ => None,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+struct DecompressedReplay {
+    path: PathBuf,
+}
 
 impl DecompressedReplay {
     fn new(path: PathBuf) -> Self {
-        Self {
-            path: path,
-            player_names: HashSet::new(),
-            team_names: HashSet::new(),
-        }
+        Self { path }
     }
 
     fn extract_strings(&self) -> Result<Vec<(String, u64)>, Box<dyn Error>> {
@@ -75,42 +115,9 @@ impl DecompressedReplay {
         rust_strings::strings(&config)
     }
 
-    fn process_strings(&mut self) -> Result<(), Box<dyn Error>> {
+    fn process_strings(&mut self) -> Result<ReplayInfo, Box<dyn Error>> {
         let extracted_strings = self.extract_strings()?;
-        for (string, _) in extracted_strings {
-            if string.starts_with("{\"v\":") {
-                let player: Result<Player, _> = serde_json::from_str(&string);
-                match player {
-                    Ok(player) => {
-                        if player.name.ends_with("...") {
-                            continue;
-                        }
-                        self.player_names.insert(player.name);
-                    },
-                    Err(e) => {
-                        eprintln!("Ignoring JSON deserialize error: {}", e);
-                        continue;
-                    }
-                };
-            }
-            else if string.starts_with("{\"t\":[\"") {
-                let team: Result<Team, _> = serde_json::from_str(&string);
-                match team {
-                    Ok(mut team) => {
-                        if team.name[0] != "teamNames" {
-                            continue;
-                        }
-                        let team_name = team.name.pop().unwrap();
-                        self.team_names.insert(team_name);
-                    },
-                    Err(e) => {
-                        eprintln!("Ignoring JSON deserialize error: {}", e);
-                        continue;
-                    }
-                };
-            }
-        }
-        Ok(())
+        Ok(ReplayInfo::new(extracted_strings))
     }
 }
 
@@ -130,17 +137,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let replay = Replay::new(input_path);
 
-    let mut decompressed_replay = unsafe {
-        replay.decompress(output_path)?
-    };
+    let mut decompressed_replay = unsafe { replay.decompress(output_path)? };
 
-    decompressed_replay.process_strings()?;
+    let processed_strings = decompressed_replay.process_strings()?;
 
-    for player_name in &decompressed_replay.player_names {
+    for player_name in &processed_strings.player_names {
         println!("{}", player_name);
     }
     println!();
-    for team_name in &decompressed_replay.team_names {
+    for team_name in &processed_strings.team_names {
         println!("{}", team_name);
     }
 
